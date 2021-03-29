efa07e031e52fe0aab0e495be37ef8b32a5f277a
rheadp <rheadpea@msu.edu>
2021-03-29 15:45:02 -0400

    commit tree b694b0a9317787ccd5bb39ce1e8b3c0d0b2ea280

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,16 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do 
+        erb :super_hero
+    end
 
+    post('/teams') do
+        @t = params['team']
+        h = params['team']['members']
+        @h1 = h[0]
+        @h2 = h[1]
+        @h3 = h[2]
+        erb :team
+end
 end
